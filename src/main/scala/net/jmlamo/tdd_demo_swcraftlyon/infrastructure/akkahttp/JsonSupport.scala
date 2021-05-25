93a49a2d63e31f01e759c0bd96b83f951a8eb285
Jean-Marie Lamodière <jmlamo@gmail.com>
2021-05-26 01:07:24 +0200

    feat: use the handler result to send an HTTP response

--- src/main/scala/net/jmlamo/tdd_demo_swcraftlyon/infrastructure/akkahttp/JsonSupport.scala
+++ src/main/scala/net/jmlamo/tdd_demo_swcraftlyon/infrastructure/akkahttp/JsonSupport.scala
@@ -2,10 +2,13 @@ package net.jmlamo.tdd_demo_swcraftlyon.infrastructure.akkahttp
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import net.jmlamo.tdd_demo_swcraftlyon.application.command.RegisterRunningSession
+import net.jmlamo.tdd_demo_swcraftlyon.domain.RunningSession
 import spray.json.DefaultJsonProtocol
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val registerRunningSessionFormat = jsonFormat2(
     RegisterRunningSession
   )
+
+  implicit val runningSessionFormat = jsonFormat4(RunningSession)
 }
