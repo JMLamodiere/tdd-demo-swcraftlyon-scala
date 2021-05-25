6f5f5e726975c870186a0add21d62a0012421cb7
Jean-Marie Lamodière <jmlamo@gmail.com>
2021-05-25 18:24:58 +0200

    feat: create a running session containing provided informations

---
+++ src/main/scala/net/jmlamo/tdd_demo_swcraftlyon/application/command/RegisterRunningSession.scala
@@ -0,0 +1,3 @@
+package net.jmlamo.tdd_demo_swcraftlyon.application.command
+
+case class RegisterRunningSession(distance: Double, shoes: String)
---
+++ src/main/scala/net/jmlamo/tdd_demo_swcraftlyon/domain/RunningSession.scala
@@ -0,0 +1,3 @@
+package net.jmlamo.tdd_demo_swcraftlyon.domain
+
+case class RunningSession(distance: Double, shoes: String)
